990a80c3f59d0ce7ff398bf322cdd758062f2d0d
faboussa <boussard.fanny@gmail.com>
2024-05-28 14:40:42 +0200

    makefile ok

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -1,3 +1,26 @@
-//
-// Created by faboussa on 5/28/24.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faboussa <faboussa@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 14:39:59 by faboussa          #+#    #+#             */
+/*   Updated: 2024/05/28 14:39:59 by faboussa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <stdbool.h>
+
+
+
+#endif
